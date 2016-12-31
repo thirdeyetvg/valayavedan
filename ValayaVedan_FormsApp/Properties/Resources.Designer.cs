2f5a46e1c3e19e76506b062b5d4249bfafa3c209
Maharajan Rathakrishnan <maharajan.thirdeye@gmail.com>
2016-12-31 08:30:02 -0800

    Updated Namespace

--- ValayaVedan_FormsApp/Properties/Resources.Designer.cs
+++ ValayaVedan_FormsApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vvtest.Properties {
+namespace valaya_vedan.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace vvtest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vvtest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("valaya_vedan.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- ValayaVedan_FormsApp/Properties/Settings.Designer.cs
+++ ValayaVedan_FormsApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vvtest.Properties {
+namespace valaya_vedan.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
